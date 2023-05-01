74b21b3000604b948648c8ea767dcb37ae7226e0
beliacitraa <21839860-beliacitraa@users.noreply.replit.com>
2023-05-01 07:00:50 +0000

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,377 @@
+<!DOCTYPE html>
+
+<head>
+  <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    
+  body {
+  background-color:antiquewhite;
+	font-family: Arial, sans-serif;
+  background:;
+  }
+  h1{
+  font-size:50px;
+  color::#fff;
+  }
+  header {
+  background-color:#fff8ee;
+  width:-moz-available;
+  position:fixed;
+  color:#734b4b;
+  text-align:center;
+  margin-top: -25px;
+  }
+  nav ul {
+	list-style: none;
+	margin: 0;
+	padding: 0;
+  color:#734b4b;
+  }  
+  nav li {
+	display: inline-block;
+	margin-right: 20px;
+  color:#fff;
+  border-radius:2px;
+  background-color:#734b4b;
+  font-size:14px;
+  }
+  nav li a {
+	color:#fff;
+	text-decoration: none;
+  }
+  main {
+  display:block;
+  max-width: 800px;
+	margin: 0 auto;
+	padding: 20px;
+  }
+  h2 {
+  padding-top: 55px;
+	font-size: 25px;
+  text-align:center;
+  margin-top:120px;
+  margin-bottom: 40px;
+  color:#734b4b;
+  }
+  .aboutt{
+  justify-content:center;
+  display:flex;
+  margin-left:20px;
+  margin-right:20px;
+  margin-top:50px;
+  color:#fff;
+  }
+  .aboutt .tabel{
+  margin-top:15px;
+  }  
+  h3{
+  font-size: 25px;
+  margin-top: 150px;
+  text-align:center;
+  }
+  .header ul {
+  border-radius: 5rem;
+  margin-top: 1rem;
+  box-shadow: 2px 0.1rem 0.5remrgb(5, 1, 0);
+  cursor: pointer;
+  border:none;
+  overflow: hidden;
+  position: relative;
+  z-index: 0;
+  list-style: none;
+	margin: 0;
+	padding: 0;
+  color:#fff;
+  }
+  li {
+	margin-bottom: 10px;
+  }
+  form label {
+	display: block;
+	margin-bottom: 5px;
+  }
+  .suggest form{
+  font-size:12px;
+  }
+  form input, form textarea {
+  font-size:12px;
+	width: 100%;
+	padding: 10px;
+	margin-bottom: 10px;
+	border-radius: 5px;
+	border: 1px solid #ccc;
+  }
+  form input[type="submit"] {
+	background-color: #000;
+	color: #fff;
+	border: none;
+	padding: 10px 20px;
+	border-radius: 20px;
+	cursor: pointer;
+  }
+ .fotoku img{
+  height: 16rem;
+  float:inline-end;
+  padding-bottom:8px;
+  }
+  .aboutt .table{
+  text-align:left;
+   margin-left:20px;
+  }
+  #about{
+  margin-left:30px;
+  margin-right:30px;
+  margin-bottom:20px;
+  }
+  footer{
+  text-align:center;  
+  }
+  .aboutt .container{
+  background-color:#5e3f3f;
+  padding-bottom: 20px;
+  padding-top: 35px;
+  padding-left:30px;
+  padding-right:60px;
+  }  
+  h3{
+  Font-size: 25px;
+  text-align: center;
+  padding-top: 45px;
+  color: #734b4b;
+  }   
+  h4{
+  Font-size: 25px;
+  text-align: center;
+  padding-top: 180px;
+  color: #734b4b;
+  }
+  h5{
+  Font-size: 25px;
+  text-align: center;
+  padding-top: 100px;
+  color: #734b4b;
+  }  
+  .porto{
+  justify-content:center;
+  display:flex;
+  color:#fff;
+  }
+  #portofolio{
+  margin-left:30px;
+  margin-right:30px;
+  margin-bottom:20px;
+  }
+  .porto .container{
+  background-color: #5e3f3f;
+  padding-bottom: 20px;
+  padding-top: 35px;
+  padding-left: 80px;
+  padding-right: 70px;
+  }
+  .suggest{
+  justify-content:center;
+  display:flex;
+  }
+  .suggest .container{
+  background-color:#5e3f3f;
+  padding-bottom: 20px;
+  margin-top: 60px;
+  padding-right: 207px;
+  padding-left: 213px;
+  color:#fff;
+  padding-top: 25px;
+  }
+  .contactt{
+  justify-content:center;
+  display:flex;
+  }
+  .contactt .container{
+  background-color:#5e3f3f;
+  padding-bottom: 20px;
+  margin-top: 40px;
+  padding-right: 207px;
+  padding-left: 213px;
+  color:#fff;
+  padding-top: 25px;
+  }  
+  .here{
+  font-size:25px;
+  text-align:center;
+  color:#fff;
+  margin-top: 195px;
+  }
+  .here img{
+  width: 315px;
+  height: 246px;
+  border: 5px solid #734b4b;
+  padding: 5px;
+  margin-top: 25px;
+  }
+  .here a{
+  margin-top:10px;
+  }  
+  </style>
+  <title>BELIA'S PORTOFOLIO</title>
+</head>
+  <body>
+	<header class="header">
+  <div class="container">
+		<h1>PORTOFOLIO</h1>
+		<nav>
+			<ul>
+				<li><a href="#about"><b>Profile</b></b></a></li>
+				<li><a href="#portofolio"><b>Portofolio</b></a></li>
+				<li><a href="#suggestions"><b>More About Me</b></a></li>
+        <li><a href="#contact"><b>Contact</b></a></li>
+			</ul>
+		</nav>
+  </header>
+  </div>
+	<main>
+  <nav class="here">
+  <img src="images/img1.jpg"> <img src="images/img2.jpg"> <img src="images/img3.jpg"> <img src="images/img5.jpg">
+  </nav>
+  <nav class="aboutt">
+  <section id="about">
+		<h2>MY PROFILE</h2>
+    <div class="container">
+     <a href="" class="fotoku">
+      <img src="images/fotoku.jpg"></a>
+			<p>Berikut ini adalah biodata saya :</p>
+    <form>
+	 <table class="tabel"> 
+		<tr>
+			<td>Nama</td>
+      <td>:</td>
+			<td>Belia Citra</td>
+		</tr>
+		<tr>
+			<td>Tempat Tanggal Lahir</td>
+      <td>:</td>
+			<td>Kayu Ara Batu, 12 Juli 2003</td>
+		</tr>
+		<tr>
+			<td>Jenis Kelamin</td>
+      <td>:</td>
+			<td>Perempuan</td>
+		</tr>
+		<tr>
+			<td>Alamat</td>
+      <td>:</td>
+			<td>Jl. Lunjuk Jaya Gg. Seroja V No.07 Palembang</td>
+		</tr>
+		<tr>
+			<td>Kewarganegaraan</td>
+      <td>:</td>
+			<td>Indonesia</td>
+		</tr>
+		<tr>
+			<td>Agama</td>
+      <td>:</td>
+			<td>Islam</td>
+		</tr>
+    </form>
+    <br>
+    </table>
+    <br>
+    <p align="justify">Saya merupakan seorang mahasiswa Universitas Sriwijaya dari Fakultas Ilmu Komputer Jurusan Sistem Informasi . Saya senang sekali menonton film dengan berbagai genre. Saya memiliki beberapa pengalaman dalam berorganisasi sejak saya duduk di bangku sekolah menengah pertama.</p>
+    </nav>
+    </div>
+  </section>
+
+  <nav class="porto">
+	<section id="portofolio">
+			<h3>MY PORTOFOLIO</h3>
+          <div class="container">	
+				<p><b>RIWAYAT PENDIDIKAN</b><p> 
+        <p>Berikut ini adalah riwayat pendidikan saya :</p>
+  <table align="center" class="tabel2">
+  <tr>
+		<td>Sekolah Dasar</td>
+    <td>:</td>
+		<td>SD Negeri 04 Muara Belida (2009-2015)</td>
+		</tr>
+		<tr>
+			<td>Sekolah Menengah Pertama</td>
+      <td>:</td>
+			<td>SMP Negeri 17 Palembang (2015-2018)</td>
+		</tr>
+    <tr>
+			<td>Sekolah Menengah Atas</td>
+      <td>:</td>
+			<td>SMA Negeri 10 Palembang (2018-2021)</td>
+		</tr>
+     <tr>
+			<td>Perguruan Tinggi</td>
+      <td>:</td>
+			<td>Universitas Sriwijaya (2021-sekarang)</td>
+		</tr>
+    </table>
+    <br>
+    <a><b>PENGALAMAN ORGANISASI</b></a>
+    <p>Berikut ini adalah pengalaman organisasi saya :</p>
+  <table align="center" class="tabel2">
+    Ekstrakulikuler Seni Tari (2015-2018)
+    <br>
+    OSIS SMP Negeri 17 Palembang (2016-2018)
+		<br>
+    Ekstrakulikuler Kelompok Ilmiah Remaja (2018-2021)
+    <br>
+    Ikatan Bujang Gadis Smanpoel (2019-2021)
+    <br>
+    Himpunan Mahasiswa Sistem Informasi (2021-sekarang)
+		<br>
+    </table>
+    </div>
+    </nav>
+  </section>
+
+  <nav class="suggest">
+		<section id="suggestions">
+			<h4>Do you have any questions?</h4>
+      <div class="container">
+      <form>
+				<label for="name">Name:</label>
+				<input type="text" id="name" name="name">
+				<label for="email">Email:</label>
+				<input type="email" id="email" name="email">
+				<label for="message">Massage:</label>
+				<textarea id="message" name="message"></textarea>
+				<input type="submit" value="Send">
+			</form>
+		</section>
+    </div>
+  </nav>
+
+  <nav class="contactt">
+    <section id="contact">
+			<h5>Contact</h5>
+      <div class="container">
+			Hubungi saya melalui :</p>
+      <table>
+    <tr>
+			<td>Email</td>
+      <td>:</td>
+			<td>beleyaactr@gmail.com</td>
+		</tr>
+    <tr>
+			<td>Instagram</td>
+      <td>:</td>
+			<td>@bectraa</td>
+		</tr>
+    <tr>
+			<td>WhatsApp</td>
+      <td>:</td>
+			<td>082269166672</td>
+		</tr>
+    </table> 
+      </div>
+      </nav>
+		</section>
+  </main>
+	<footer>
+		<p>&copy; 2023 Belia Citra. All rights reserved.</p>
+	</footer>
+</body>
+</html
